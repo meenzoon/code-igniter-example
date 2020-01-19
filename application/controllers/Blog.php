194faaa90cba891d790c40f414aae56216d733dc
meenzoon <rlaalswns523@gmail.com>
2020-01-20 00:11:08 +0900

    1. Welcome.php   -  comments 메소드 추가 2. Blog.php   - create 메소드 추가

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -1,8 +1,20 @@
 <?php
 class Blog extends CI_Controller {
 
+	function __construct()
+	{
+		parent::__construct();
+	}
+
 	public function index()
 	{
 		echo 'Hello Blog';
+	}
+
+	public function create($title, $id)
+	{
+		echo $title;
+		echo '<br>';
+		echo $id;
 	}
 }
--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Main extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -10,12 +10,12 @@ class Welcome extends CI_Controller {
 	 * 		http://example.com/index.php/welcome
 	 *	- or -
 	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
+*	- or -
+* Since this controller is set as the default controller in
+* config/routes.php, it's displayed at http://example.com/
 	 *
 	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
+	 * map to /Main.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index()
